7e3961d459693bc5040f5b6408bae82f8ab1a72a
Elvis Gilbert Batista Pimentel <44851784+Kakuhiry@users.noreply.github.com>
2020-09-05 17:53:35 -0400

    Component based files organization

---
+++ src/Components/LandingPage.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import '/home/elvis/Documents/GitHub/medbase/src/Styles/LangingPageStyle.css';
+
+function LandingPage() {
+  return (
+    <div className="LandingPage">
+      <div className= "grad">
+        <div className = "header">
+          <img className="logo" src={require('/home/elvis/Documents/GitHub/medbase/src/images/3533224112_e75c18de-aef7-47fb-bf36-5748bd89b1bf.png')} alt= "Not found"/>
+          <div className= "nav-bar-left">
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="/">About</a></li>
+              <li><a href="/">Contact</a></li>
+            </ul>
+          </div>
+          <div className= "nav-bar-right">
+          <ul>
+            <li>
+              <a href="/">Login</a>
+            </li>
+          </ul>
+          </div>
+        </div>
+        <div className= "main-body">
+          <div className= "main-body-left">
+            <h1 className= "main-text-left">
+            Acceso a tu salud, no importa donde vayas
+            </h1>
+            <p className= "secondary-text-left">
+            Tu salud es muy importante para nosotros. Nuestro objetivo es proveer al publico un facil acceso a su historial medico, sin importar donde se encuentre.
+            </p>
+            </div>
+          <div className= "main-body-right">
+            <img className="right-pic" src={require('/home/elvis/Documents/GitHub/medbase/src/images/fimathing.png')} alt= "Not found"/>
+          </div>
+        </div>
+          <div class="box-1">
+            <div class="btn btn-one">
+              <span>Enroll today</span>
+            </div>
+          </div>
+          <div className= "socials">
+            <ul>
+              <li><img className= "sLogo" id= "fLogo" src={require("/home/elvis/Documents/GitHub/medbase/src/images/facebook-logo.png")} alt="1"/></li>
+              <li><img className= "sLogo" id= "iLogo" src={require("/home/elvis/Documents/GitHub/medbase/src/images/Logo-instagram.png")} alt="2"/></li>
+              <li><img className= "sLogo" id= "tLogo" src={require("/home/elvis/Documents/GitHub/medbase/src/images/twitter-logo.png")} alt="3"/></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export default LandingPage;
